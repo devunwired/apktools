e2742dae713112efcecffff099ea0a32d9c39327
Dave Smith <smith@wiresareobsolete.com>
2012-08-09 20:46:00 -0600

    Initial commit.

---
+++ lib/apktools/apkresources.rb
@@ -0,0 +1,557 @@
+#
+# Author: Dave Smith
+#
+# Class to parse an APK's resources.arsc data and retrieve resource
+# data associated with a given R.id value
+
+require 'rubygems'
+require 'zip/zip'
+
+DEBUG = true
+
+# Define Structures
+ChunkHeader = Struct.new(:type, :size, :chunk_size)
+StringPool = Struct.new(:header, :string_count, :style_count, :values)
+PackageHeader = Struct.new(:header, :id, :name, :type_strings, :key_strings)
+ResTypeSpec = Struct.new(:header, :id, :entry_count, :entries, :types)
+ResType = Struct.new(:header, :id, :config, :entry_count, :entries)
+ResTypeConfig = Struct.new(:imsi, :locale, :screen_type, :input, :screen_size, :version, :screen_config, :screen_size_dp)
+ResTypeEntry = Struct.new(:flags, :key, :data_type, :data)
+
+# Type Constants
+TYPE_ARRAY = "array"
+TYPE_ATTRIBUTE = "attr"
+TYPE_BOOLEAN = "bool"
+TYPE_COLOR = "color"
+TYPE_DIMENSION = "dimen"
+TYPE_DRAWABLE = "drawable"
+TYPE_FRACTION = "fraction"
+TYPE_INTEGER = "integer"
+TYPE_LAYOUT = "layout"
+TYPE_PLURALS = "plurals"
+TYPE_STRING = "string"
+TYPE_STYLE = "style"
+
+# Data Type Constants
+TYPE_INT_DEC = 0x10
+TYPE_INT_HEX = 0x11
+TYPE_BOOL = 0x12
+TYPE_INT_COLOR_RGB4 = 0x1F
+TYPE_INT_COLOR_ARGB4 = 0x1E
+TYPE_INT_COLOR_RGB8 = 0x1D
+TYPE_INT_COLOR_ARGB8 = 0x1C
+COMPLEX_UNIT_PX = 0x0
+COMPLEX_UNIT_DIP = 0x1
+COMPLEX_UNIT_SP = 0x2
+COMPLEX_UNIT_PT = 0x3
+COMPLEX_UNIT_IN = 0x4
+COMPLEX_UNIT_MM = 0x5
+
+# Configuration Constants
+ACONFIGURATION_ORIENTATION_ANY  = 0x0000
+ACONFIGURATION_ORIENTATION_PORT = 0x0001
+ACONFIGURATION_ORIENTATION_LAND = 0x0002
+ACONFIGURATION_ORIENTATION_SQUARE = 0x0003
+
+ACONFIGURATION_TOUCHSCREEN_ANY  = 0x0000
+ACONFIGURATION_TOUCHSCREEN_NOTOUCH  = 0x0001
+ACONFIGURATION_TOUCHSCREEN_STYLUS  = 0x0002
+ACONFIGURATION_TOUCHSCREEN_FINGER  = 0x0003
+
+ACONFIGURATION_DENSITY_DEFAULT = 0
+ACONFIGURATION_DENSITY_LOW = 120
+ACONFIGURATION_DENSITY_MEDIUM = 160
+ACONFIGURATION_DENSITY_TV = 213
+ACONFIGURATION_DENSITY_HIGH = 240
+ACONFIGURATION_DENSITY_XHIGH = 320
+ACONFIGURATION_DENSITY_XXHIGH = 480
+ACONFIGURATION_DENSITY_NONE = 0xffff
+
+ACONFIGURATION_KEYBOARD_ANY  = 0x0000
+ACONFIGURATION_KEYBOARD_NOKEYS  = 0x0001
+ACONFIGURATION_KEYBOARD_QWERTY  = 0x0002
+ACONFIGURATION_KEYBOARD_12KEY  = 0x0003
+
+ACONFIGURATION_NAVIGATION_ANY  = 0x0000
+ACONFIGURATION_NAVIGATION_NONAV  = 0x0001
+ACONFIGURATION_NAVIGATION_DPAD  = 0x0002
+ACONFIGURATION_NAVIGATION_TRACKBALL  = 0x0003
+ACONFIGURATION_NAVIGATION_WHEEL  = 0x0004
+
+ACONFIGURATION_KEYSHIDDEN_ANY = 0x0000
+ACONFIGURATION_KEYSHIDDEN_NO = 0x0001
+ACONFIGURATION_KEYSHIDDEN_YES = 0x0002
+ACONFIGURATION_KEYSHIDDEN_SOFT = 0x0003
+
+ACONFIGURATION_NAVHIDDEN_ANY = 0x0000
+ACONFIGURATION_NAVHIDDEN_NO = 0x0001
+ACONFIGURATION_NAVHIDDEN_YES = 0x0002
+
+ACONFIGURATION_SCREENSIZE_ANY  = 0x00
+ACONFIGURATION_SCREENSIZE_SMALL = 0x01
+ACONFIGURATION_SCREENSIZE_NORMAL = 0x02
+ACONFIGURATION_SCREENSIZE_LARGE = 0x03
+ACONFIGURATION_SCREENSIZE_XLARGE = 0x04
+
+ACONFIGURATION_SCREENLONG_ANY = 0x00
+ACONFIGURATION_SCREENLONG_NO = 0x1
+ACONFIGURATION_SCREENLONG_YES = 0x2
+
+ACONFIGURATION_UI_MODE_TYPE_ANY = 0x00
+ACONFIGURATION_UI_MODE_TYPE_NORMAL = 0x01
+ACONFIGURATION_UI_MODE_TYPE_DESK = 0x02
+ACONFIGURATION_UI_MODE_TYPE_CAR = 0x03
+ACONFIGURATION_UI_MODE_TYPE_TELEVISION = 0x04
+ACONFIGURATION_UI_MODE_TYPE_APPLIANCE = 0x05
+
+ACONFIGURATION_UI_MODE_NIGHT_ANY = 0x00
+ACONFIGURATION_UI_MODE_NIGHT_NO = 0x1
+ACONFIGURATION_UI_MODE_NIGHT_YES = 0x2
+
+ACONFIGURATION_SCREEN_WIDTH_DP_ANY = 0x0000
+
+ACONFIGURATION_SCREEN_HEIGHT_DP_ANY = 0x0000
+
+ACONFIGURATION_SMALLEST_SCREEN_WIDTH_DP_ANY = 0x0000
+
+# Spec Configuration Constants
+ACONFIGURATION_MCC = 0x0001
+ACONFIGURATION_MNC = 0x0002
+ACONFIGURATION_LOCALE = 0x0004
+ACONFIGURATION_TOUCHSCREEN = 0x0008
+ACONFIGURATION_KEYBOARD = 0x0010
+ACONFIGURATION_KEYBOARD_HIDDEN = 0x0020
+ACONFIGURATION_NAVIGATION = 0x0040
+ACONFIGURATION_ORIENTATION = 0x0080
+ACONFIGURATION_DENSITY = 0x0100
+ACONFIGURATION_SCREEN_SIZE = 0x0200
+ACONFIGURATION_VERSION = 0x0400
+ACONFIGURATION_SCREEN_LAYOUT = 0x0800
+ACONFIGURATION_UI_MODE = 0x1000
+ACONFIGURATION_SMALLEST_SCREEN_SIZE = 0x2000
+
+# Data Constants
+TYPE_BOOL_TRUE = 0xFFFFFFFF
+TYPE_BOOL_FALSE = 0x00000000
+
+CHUNKTYPE_TYPESPEC = 0x202
+CHUNKTYPE_TYPE = 0x201
+
+#Flag Constants
+FLAG_UTF8 = 0x100
+
+OFFSET_NO_ENTRY = 0xFFFFFFFF
+HEADER_START = 0
+
+class ApkResources
+
+	attr_accessor :stringpool_main, :package_header, :stringpool_typestrings, :stringpool_keystrings, :type_data
+
+	# Initialize resources data from file
+	def initialize(apk_file)
+		data = nil		
+		# Get resources.arsc from the APK file
+		Zip::ZipFile.foreach(apk_file) do |f|
+		  if f.name.match(/resources.arsc/)
+			data = f.get_input_stream.read
+		  end
+		end
+		
+		# Parse the Table Chunk
+		## Header
+		header_type = read_short(data, HEADER_START)
+		header_size = read_short(data, HEADER_START+2)
+		header_chunk_size = read_word(data, HEADER_START+4)
+		header_package_count = read_word(data, HEADER_START+8)
+		puts "Resource Package Count = #{header_package_count}" if DEBUG
+		if header_package_count > 1
+			puts "ApkResources only supports single package resources."
+			exit(1)
+		end
+		
+		# Parse the StringPool Chunk
+		## Header
+		startoffset_pool = HEADER_START + header_size
+		puts "Parse Main StringPool Chunk" if DEBUG
+		@stringpool_main = parse_stringpool(data, startoffset_pool)
+		puts "#{@stringpool_main.values.length} strings found" if DEBUG
+		
+		# Parse the Package Chunk
+		## Header
+		startoffset_package = startoffset_pool + @stringpool_main.header.chunk_size
+		header = ChunkHeader.new( read_short(data, startoffset_package),
+				read_short(data, startoffset_package+2),
+				read_word(data, startoffset_package+4) )
+		
+		package_id = read_word(data, startoffset_package+8)
+		package_name = read_string(data, startoffset_package+12, 256, "UTF-8")
+		package_type_strings = read_word(data, startoffset_package+268)
+		package_last_type = read_word(data, startoffset_package+272)
+		package_key_strings = read_word(data, startoffset_package+276)
+		package_last_key = read_word(data, startoffset_package+280)
+		
+		@package_header = PackageHeader.new(header, package_id, package_name, package_type_strings, package_key_strings)
+		
+		## typeStrings StringPool
+		startoffset_typestrings = startoffset_package + package_type_strings
+		puts "Parse typeStrings StringPool Chunk" if DEBUG
+		@stringpool_typestrings = parse_stringpool(data, startoffset_typestrings)
+		
+		## keyStrings StringPool
+		startoffset_keystrings = startoffset_package + package_key_strings
+		puts "Parse keyStrings StringPool Chunk" if DEBUG
+		@stringpool_keystrings = parse_stringpool(data, startoffset_keystrings)
+		
+		## typeSpec/type Chunks		
+		@type_data = Array.new()
+		current_spec = nil
+		
+		current = startoffset_keystrings + @stringpool_keystrings.header.chunk_size
+		puts "Parse Type/TypeSpec Chunks" if DEBUG
+		while current < data.length
+			## Parse Header
+			header = ChunkHeader.new( read_short(data, current),
+					read_short(data, current+2),
+					read_word(data, current+4) )
+			## Check Type
+			if header.type == CHUNKTYPE_TYPESPEC
+				typespec_id = read_byte(data, current+8)
+				typespec_entrycount = read_word(data, current+12)
+				
+				## Parse the config flags for each entry
+				typespec_entries = Array.new()
+				i=0
+				while i < typespec_entrycount
+					offset = i * 4 + (current+16)
+					typespec_entries << read_word(data, offset)
+					
+					i += 1
+				end
+				
+				typespec_name = @stringpool_typestrings.values[typespec_id - 1]
+				current_spec = ResTypeSpec.new(header, typespec_name, typespec_entrycount, typespec_entries, nil)
+				
+				@type_data << current_spec
+				current += header.chunk_size
+			elsif header.type == CHUNKTYPE_TYPE
+				type_id = read_byte(data, current+8)
+				type_entrycount = read_word(data, current+12) 
+				type_entryoffset = read_word(data, current+16)
+
+				## The config flags set for this type chunk
+				## TODO: Vary the size of the config structure based on size to accomodate for new flags
+				config_size = read_word(data, current+20) # Number of bytes in structure
+				type_config = ResTypeConfig.new( read_word(data, current+24),
+						read_word(data, current+28),
+						read_word(data, current+32),
+						read_word(data, current+36 ),
+						read_word(data, current+40),
+						read_word(data, current+44),
+						read_word(data, current+48),
+						read_word(data, current+52) )
+
+				## The remainder of the chunk is a list of the entry values for that type/configuration
+				type_name = @stringpool_typestrings.values[type_id - 1]				
+				if current_spec.types == nil
+					current_spec.types = ResType.new(header, type_name, type_config, type_entrycount, Array.new())
+				end
+
+				i=0
+				while i < type_entrycount
+					## Ensure a hash exists for each type
+					if current_spec.types.entries[i] == nil
+						current_spec.types.entries[i] = Hash.new()
+					end
+					current_entry = current_spec.types.entries[i]
+
+					## Get the start of the type from the offsets table
+					index_offset = i * 4 + (current+56)
+					start_offset = read_word(data, index_offset)
+					if start_offset != OFFSET_NO_ENTRY
+						## Set the index_offset to the start of the current entry
+						index_offset = current + type_entryoffset + start_offset
+	
+						entry_flags = read_short(data, index_offset+2)
+						entry_key = read_word(data, index_offset+4)
+						entry_data_type = read_byte(data, index_offset+11)
+						entry_data = read_word(data, index_offset+12)
+						
+						# Find the key in our strings index
+						key_name = @stringpool_keystrings.values[entry_key]
+						# Parse the value into a string
+						data_value = nil
+						case type_name
+							when TYPE_STRING, TYPE_DRAWABLE
+								data_value = get_resource_string(entry_data_type, entry_data)
+							when TYPE_COLOR
+								data_value = get_resource_color(entry_data_type, entry_data)
+							when TYPE_DIMENSION
+								data_value = get_resource_dimension(entry_data_type, entry_data)
+							when TYPE_INTEGER
+								data_value = get_resource_integer(entry_data_type, entry_data)
+							when TYPE_BOOLEAN
+								data_value = get_resource_bool(entry_data_type, entry_data)
+							else
+								puts "Complex Resources not yet supported." if DEBUG
+								data_value = entry_data.to_s
+						end
+						current_entry[type_config] = ResTypeEntry.new(entry_flags, key_name, entry_data_type, data_value)
+					end
+					i += 1
+				end
+				
+				current += header.chunk_size
+			else
+				puts "Unknown Chunk Found: #{header.type} #{header.size}" if DEBUG
+				## End Immediately
+				current = data.length
+			end
+		end
+		
+	end #initalize
+	
+	# Return array of all string values in the file
+	def get_all_strings
+		return @stringpool_main.values
+	end
+	
+	# Return array of all the type values in the file
+	def get_all_types
+		return @stringpool_typestrings.values
+	end
+	
+	# Return array of all the key values in the file
+	def get_all_keys
+		return @stringpool_keystrings.values
+	end
+
+	# Obtain the key value for a given resource id
+	#
+	# res_id: ID value of a resource as a FixNum or String representation (i.e. 0x7F060001)
+	# xml_format: Optionally format return string for XML files.
+	#
+	# If xml_format is true, return value will be @<type>/<key>
+	# If xml_format is false or missing, return value will be R.<type>.<key>
+	def get_resource_key(res_id, xml_format=false)
+		if res_id.is_a? String
+			res_id = res_id.hex
+		end
+		
+		# R.id integers are a concatenation of package_id, type_id, and entry index
+		res_package = (res_id >> 24) & 0xFF
+		res_type = (res_id >> 16) & 0xFF
+		res_index = res_id & 0xFFFF
+
+		if res_package != @package_header.id
+			# This is not a resource we can parse
+			return res_id_to_s(res_id)
+		end
+		
+		res_spec = @type_data[res_type-1]
+		entry = res_spec.types.entries[res_index]
+		
+		if entry == nil
+			# There is no entry in our table for this resource
+			return res_id_to_s(res_id)
+		end
+		
+		if xml_format
+			return "@#{res_spec.id}/#{entry.values[0].key}"
+		else
+			return "R.#{res_spec.id}.#{entry.values[0].key}"
+		end
+	end
+
+	# Obtain the default value for a given resource id
+	#
+	# res_id: ID values of a resources as a FixNum or String representation (i.e. 0x7F060001)
+	#
+	# Returns: The default ResTypeEntry to the given id, or nil if no default exists
+	def get_default_resource_value(res_id)
+		entries = get_resource_value(res_id)
+		default = ResTypeConfig.new(0, 0, 0, 0, 0, 0, 0, 0)
+		
+		return entries[default]
+	end
+
+	# Obtain the value(s) for a given resource id.
+	# A default resource is one defined in an unqualified directory.
+	#
+	# res_id: ID value of a resource as a FixNum or String representation (i.e. 0x7F060001)
+	#
+	# Returns: Hash of all entries matching this id, keyed by their matching ResTypeConfig
+	#
+	# TODO: Implement a return scheme for complex resources (arrays, styles, plurals, etc.)
+	def get_resource_value(res_id)
+		if res_id.is_a? String
+			res_id = res_id.hex
+		end
+		
+		# R.id integers are a concatenation of package_id, type_id, and entry index
+		res_package = (res_id >> 24) & 0xFF
+		res_type = (res_id >> 16) & 0xFF
+		res_index = res_id & 0xFFFF
+
+		if res_package != @package_header.id
+			# This is not a resource we can parse
+			return res_id_to_s(res_id)
+		end
+		
+		res_spec = @type_data[res_type-1]
+		
+		entries = res_spec.types.entries[res_index]
+		if entries == nil
+			puts "Could not find #{type_name} ResType chunk" if DEBUG
+			return res_id_to_s(res_id)
+		end
+		
+		return entries
+	end
+
+	private # Private Helper Methods
+
+	# Read a 32-bit word from a specific location in the data
+	def read_word(data, offset)
+	  out = data[offset,4].unpack('V').first rescue 0
+	  return out
+	end
+	
+	# Read a 16-bit short from a specific location in the data
+	def read_short(data, offset)
+	  out = data[offset,2].unpack('v').first rescue 0
+	  return out
+	end
+		
+	# Read a 8-bit byte from a specific location in the data
+	def read_byte(data, offset)
+	  out = data[offset,1].unpack('C').first rescue 0
+	  return out
+	end
+
+	# Read in length bytes in as a String
+	def read_string(data, offset, length, encoding)
+		if "UTF-16".casecmp(encoding) == 0
+			out = data[offset, length].unpack('v*').pack('U*')
+		else
+			out = data[offset, length].unpack('C*').pack('U*')
+		end
+	  return out
+	end
+	
+	# Return id as a hex string
+	def res_id_to_s(res_id)
+		return "0x#{res_id.to_s(16)}"
+	end
+	
+	# Parse out a StringPool chunk
+	#TODO: Add styled strings
+	def parse_stringpool(data, offset)
+		pool_header = ChunkHeader.new( read_short(data, offset),
+				read_short(data, offset+2),
+				read_word(data, offset+4) )
+	
+		pool_string_count = read_word(data, offset+8)
+		pool_style_count = read_word(data, offset+12)
+		pool_flags = read_word(data, offset+16)
+		format_utf8 = (pool_flags & FLAG_UTF8) != 0
+		puts 'StringPool format is %s' % [format_utf8 ? "UTF-8" : "UTF-16"] if DEBUG
+
+		pool_string_offset = read_word(data, offset+20)
+		pool_style_offset = read_word(data, offset+24)
+		
+		values = Array.new()
+		i = 0
+		while i < pool_string_count
+			# Read the string value
+			index = i * 4 + (offset+28)
+			offset_addr = pool_string_offset + offset + read_word(data, index)
+			if format_utf8
+				length = read_byte(data, offset_addr)
+				if (length & 0x80) != 0
+					length = ((length & 0x7F) << 8) + read_byte(data, offset_addr+1)
+				end
+				
+				values << read_string(data, offset_addr + 2, length, "UTF-8")
+			else
+				length = read_short(data, offset_addr)
+				if (length & 0x8000) != 0
+					#There is one more length value before the data
+					length = ((length & 0x7FFF) << 16) + read_short(data, offset_addr+2)
+					values << read_string(data, offset_addr + 4, length * 2, "UTF-16")
+				else
+					# Read the data
+					values << read_string(data, offset_addr + 2, length * 2, "UTF-16")
+				end
+			end	
+		
+			i += 1
+		end
+		
+		return StringPool.new(pool_header, pool_string_count, pool_style_count, values)
+	end
+
+	# Obtain string value for resource id
+	def get_resource_string(entry_datatype, entry_data)
+		result = @stringpool_main.values[entry_data]
+		return result
+	end
+	
+	# Obtain boolean value for resource id
+	def get_resource_bool(entry_datatype, entry_data)
+		if entry_data == TYPE_BOOL_TRUE
+			return "true"
+		elsif entry_data == TYPE_BOOL_FALSE
+			return "false"
+		else
+			return "undefined"
+		end	
+	end
+	
+	# Obtain integer value for resource id
+	def get_resource_integer(entry_datatype, entry_data)
+		if entry_datatype == TYPE_INT_HEX
+			return "0x#{entry_data.to_s(16)}"
+		else
+			return entry_data.to_s
+		end
+	end
+
+	# Obtain color value for resource id
+	def get_resource_color(entry_datatype, entry_data)
+		case entry_datatype
+		when TYPE_INT_COLOR_RGB4
+			return "#" + ((entry_data >> 16) & 0xF).to_s(16) + ((entry_data >> 8) & 0xF).to_s(16) + (entry_data & 0xF).to_s(16)
+		when TYPE_INT_COLOR_ARGB4
+			return "#" + ((entry_data >> 24) & 0xF).to_s(16) + ((entry_data >> 16) & 0xF).to_s(16) + ((entry_data >> 8) & 0xF).to_s(16) + (entry_data & 0xF).to_s(16)
+		when TYPE_INT_COLOR_RGB8
+			return "#" + ((entry_data >> 16) & 0xFF).to_s(16) + ((entry_data >> 8) & 0xFF).to_s(16) + (entry_data & 0xFF).to_s(16)
+		when TYPE_INT_COLOR_ARGB8
+			return "#" + ((entry_data >> 24) & 0xFF).to_s(16) + ((entry_data >> 16) & 0xFF).to_s(16) + ((entry_data >> 8) & 0xFF).to_s(16) + (entry_data & 0xFF).to_s(16)
+		else
+			return "0x#{entry_data.to_s(16)}"
+		end
+	end
+	
+	# Obtain dimension value for resource id
+	def get_resource_dimension(entry_datatype, entry_data)
+		unit_type = (entry_data & 0xFF)
+		case unit_type
+		when COMPLEX_UNIT_PX
+			unit_name = "px"
+		when COMPLEX_UNIT_DIP
+			unit_name = "dp"
+		when COMPLEX_UNIT_SP
+			unit_name = "sp"
+		when COMPLEX_UNIT_PT
+			unit_name = "pt"
+		when COMPLEX_UNIT_IN
+			unit_name = "in"
+		when COMPLEX_UNIT_MM
+			unit_name = "mm"
+		else
+			unit_name = ""
+		end
+		
+		return ((entry_data >> 8) & 0xFFFFFF).to_s + unit_name
+	end	
+end
